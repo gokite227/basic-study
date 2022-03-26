339608efe74ca7dbb02cb65e9c9ff73ac7fa7bdf
gokite227 <102020249+gokite227@users.noreply.github.com>
2022-03-26 22:12:35 +0900

    Add files via upload

---
+++ python/5.for/for8.py
@@ -0,0 +1,8 @@
+#리스트 요소의 값을 반복문을 사용하여 모두 출력 하시오
+#이때 가로로 값을 출력 시키시오
+#값을 모두 출력한 후에는 끝에 요소의 갯수를 함께 출력 시키시오
+
+lst= [ 'dog', 'hippo', 'elephant', 'lion', 'tiger', 'alligator']
+
+for i in lst:
+    print(i, " ", end = "") 
