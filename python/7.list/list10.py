a7a52658f7489634f73128eabf0cca9576c178b8
gokite227 <102020249+gokite227@users.noreply.github.com>
2022-03-29 20:24:03 +0900

    Add files via upload

---
+++ python/7.list/list10.py
@@ -0,0 +1,10 @@
+#학생들의 영어 점수 리스트에 새 전학생의 영어 점수를 추가 해보시오
+
+eng_scores = [ 90, 60, 75, 100, 88 ]
+print(eng_scores)
+eng_scores.append(99)
+print(eng_scores)
+eng_scores[5] = 37
+print(eng_scores)
+eng_scores.remove(37)
+print(eng_scores)
