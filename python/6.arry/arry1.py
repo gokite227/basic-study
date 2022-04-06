2e52c679b7c013ac910cc81e80d5907a90b3fe15
gokite227 <102020249+gokite227@users.noreply.github.com>
2022-04-06 22:00:34 +0900

    Create arry1.py

---
+++ python/6.arry/arry1.py
@@ -0,0 +1,19 @@
+# (1) 수행
+numbers = [205, 115, 155, 94, 561, 110]
+print(numbers)
+
+# (2) 수행
+numbers.remove(155)
+print(numbers)
+
+# (3) 수행
+numbers.insert(2, 755)
+print(numbers)
+
+# (4) 수행
+numbers.reverse()
+print(numbers)
+
+# (5) 수행
+numbers.pop()
+print(numbers)
