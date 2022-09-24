e108f3bcccf12ba698470b393ce623f33e701e20
gokite227 <102020249+gokite227@users.noreply.github.com>
2022-09-25 03:13:16 +0900

    useEffect

--- react-forbeginners/src/App.js
+++ react-forbeginners/src/App.js
@@ -1,11 +1,31 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import userEvent from "@testing-library/user-event";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev)=>prev +1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(()=> {
+    console.log("CALL THE API...");
+  }, []);
+  useEffect(() => {
+    console.log(" I run when 'keyword' changes.");
+  },[keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(()=> {
+    console.log("I run when keyword&counter changes.")
+  }, [keyword,counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={'Continue'} />
+      <input value={keyword}
+      onChange={onChange}
+      type="text"
+      placeholder="Serch here..." />
+      <h1 >{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
