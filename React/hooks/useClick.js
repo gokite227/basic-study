2edfc994f1402486f81995718283534140a700e2
gokite227 <102020249+gokite227@users.noreply.github.com>
2022-09-26 01:19:08 +0900

    Recact useClick

---
+++ React/hooks/useClick.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+export const useClick = (onClick) => {
+    if (typeof onClick !== "function") {
+        return;
+    }
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+
+export default function App() {
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+}
