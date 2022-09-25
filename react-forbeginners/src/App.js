952aa814285a254d5866ba171666001c47402d9a
gokite227 <102020249+gokite227@users.noreply.github.com>
2022-09-25 22:09:55 +0900

    React movie

--- react-forbeginners/src/App.js
+++ react-forbeginners/src/App.js
@@ -1,31 +1,24 @@
-import userEvent from "@testing-library/user-event";
-import {useState, useEffect} from "react";
+import * as React from "react";
+import {
+  BrowserRouter as Roter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
     return (
-        <div>
-          <h1>The Coins!{loading? "" : `(${coins.length})`}</h1>
-          {loading ? <strong>Loding...</strong> :
-          <select>
-          {coins.map((coin) => (
-              <option>
-                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-              </option>
-            ))}  
-          </select>
-          } 
-        </div>
-    );
+    <Roter>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Roter>);
 }
 
 export default App;
---
+++ react-forbeginners/src/routes/Detail.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import{useParams} from "react-router-dom";
+import Movie from "../components/Movie";
+
+function Detail() {
+    const {id} = useParams();
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+    }
+    useEffect(() => {
+        getMovie();
+    },[]);
+    return (
+    <div>
+        <h1>
+        Detail
+        </h1>
+    </div>);
+}
+
+export default Detail;
